37bf5e75249f69e441be68877fc482c372f2b93d
andrewhoch <andrew-hoch@uiowa.edu>
2014-10-23 18:40:37 -0500

    Create Indexer.java

---
+++ src/miniSearchEngine/Indexer.java
@@ -0,0 +1,29 @@
+package miniSearchEngine;
+
+public class Indexer {
+	
+	public String identifier;
+	
+	public Indexer(String id){
+		this.identifier = id;
+	}
+	
+	public String getIdentifier(){
+		return identifier;
+	}	
+	
+	public void add(Field f) {
+		IndexerBag ib = new IndexerBag(this,f); 
+		DataBase.addToData(ib);
+	}
+	
+	public void remove(Field f){
+		IndexerBag ib = new IndexerBag(this, f);
+		DataBase.removeFromData(ib);
+	}
+		
+	public void close() {
+		
+	}
+	
+}
